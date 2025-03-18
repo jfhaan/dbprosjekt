ceb6629ed4f728b01ddeff4b5108f6621d4d1475
Martin Nguyen <martinng@stud.ntnu.no>
2025-03-18 14:48:46 +0100

    Add: insert data to Flyselskap
    Added data to the table containing Flyselskap

--- data.sql
+++ data.sql
@@ -14,3 +14,12 @@ VALUES ('SVG', 'Stavanger Lufthavn, Sola');
 INSERT INTO Flyplass (Kode, Navn)
 VALUES ('TRD', 'Trondheim Lufthavn, Værnes');
 
+-- Brukstilfelle 2.1: Registrere flyselskap --
+INSERT INTO Flyselskap (Kode, Navn)
+VALUES ('DY', 'Norwegian');
+
+INSERT INTO Flyselskap (Kode, Navn)
+VALUES ('SK', 'SAS');
+
+INSERT INTO  Flyselskap (Kode, Navn)
+VALUES ('WF', 'Widerøe');

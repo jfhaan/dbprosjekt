d9cbdb0aea9917733c89a63750895b6d3963cfdb
Martin Nguyen <martinng@stud.ntnu.no>
2025-03-18 14:45:02 +0100

    Add: Added use case 1
    Added SQL for inserting to Flyplass

---
+++ data.sql
@@ -0,0 +1,16 @@
+-- Brukstilfelle 1: Registrere flyplass --
+INSERT INTO Flyplass (Kode, Navn)
+VALUES ('BOO', 'Bodø Lufthavn');
+
+INSERT INTO Flyplass (Kode, Navn)
+VALUES ('BGO', 'Bergen Lufthavn, Flesland');
+
+INSERT INTO Flyplass (Kode, Navn)
+VALUES ('OSL', 'Oslo Lufthavn, Gardermoen');
+
+INSERT INTO Flyplass (Kode, Navn)
+VALUES ('SVG', 'Stavanger Lufthavn, Sola');
+
+INSERT INTO Flyplass (Kode, Navn)
+VALUES ('TRD', 'Trondheim Lufthavn, Værnes');
+

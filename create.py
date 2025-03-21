ef1ba23f119bdcbf6e5368808c3bbc17b37d720a
Erlend Tryti Lindseth <etl@dhcp-10-24-0-147.wlan.ntnu.no>
2025-03-21 09:42:39 +0100

    clean

---
+++ create.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+
+# Read and execute SQL script
+def execute(cursor, sql_script):
+    try:
+        with open(sql_script, "r") as file:
+            sql_script_content = file.read()
+        cursor.executescript(sql_script_content)
+        print(f"Successfully executed {sql_script}")
+    except Exception as e:
+        print(f"Error executing SQL script: {e}")
+
+
+def main():
+    conn = sqlite3.connect("fly.db")
+    c = conn.cursor()
+
+    execute(c, "schema.sql")
+    execute(c, "data.sql")
+
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()

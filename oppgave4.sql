9a052b3bc51f88c7761ebde89703165a0260886e
Johannes de Haan <jmfudh@icloud.com>
2025-03-21 13:06:50 +0100

    Fikset feil Id på BOO-TRD

--- oppgave4.sql
+++ oppgave4.sql
@@ -1,5 +1,5 @@
 INSERT INTO Flyvning(Loepenummer, RuteStrekningId, FlyvningStatus, FaktiskAvgang, FaktiskAnkomst, Fly)
-VALUES ('WF1302 01APR2025', 1, 'planned', '2025-04-01 15:15:00', '2025-04-01 16:20:00', 'LN-WIH'),
+VALUES ('WF1302 01APR2025', 2, 'planned', '2025-04-01 07:35:00', '2025-04-01 08:40:00', 'LN-WIH'),
        ('DY753 01APR2025', 3 , 'planned', '2025-04-01 10:20:00', '2025-04-01 11:15:00', 'LN-ENU'),
        ('SK888 01APR2025', 5 , 'planned', '2025-04-01 10:00:00', '2025-04-01 11:10:00', 'SE-RUB'),
        ('SK888 01APR2025', 6 , 'planned', '2025-04-01 11:40:00', '2025-04-01 12:10:00', 'SE-RUB'),
